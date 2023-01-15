10fb805aa6bb037565e6f4b42d6646923504e531
Timothy Lee <leetim1202@gmail.com>
2023-01-15 08:29:57 +0000

    prep-javascript-arrays Timothy Lee

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+
+var firstElement = languages[0];
+console.log('The first element of this array is ' + firstElement);
+
+var thirdElement = languages[2];
+console.log('The third element of this array is ' + thirdElement);
+
+var length = languages.length;
+console.log('The length of this array is ' + length);
+
+var lastElement = languages[5];
+console.log('The last element of this array is ' + lastElement);
