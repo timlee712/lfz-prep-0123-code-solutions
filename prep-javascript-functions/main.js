1363c5810a1aa5a2f956ed55a86cadb224b4c681
Timothy Lee <leetim1202@gmail.com>
2023-01-18 04:59:01 +0000

    prep-javascript-functions Timothy Lee

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,53 @@
+function addTwoNumbers(a, b) {
+  return a + b;
+}
+var addTwoNumbersResults = addTwoNumbers(2, 2);
+console.log('addTwoNumbers exercise: ', addTwoNumbersResults);
+
+function convertHoursToMinutes(a) {
+  return a * 60;
+}
+var convertHoursToMinutesResults = convertHoursToMinutes(2);
+console.log('convertHoursToMinutes exercise: ', convertHoursToMinutesResults);
+
+function getGreeting(name) {
+  return 'Hello ' + name + '!';
+}
+var getGreetingResults = getGreeting('Tim');
+console.log('getGreeting exercise: ', getGreetingResults);
+
+function addAndMultiplyBy5(a, b) {
+  return (a + b) * 5;
+}
+var addAndMultiplyBy5Results = addAndMultiplyBy5(10, 5);
+console.log('addAndMultiplyBy5 exercise: ', addAndMultiplyBy5Results);
+
+function multiplyAndDivideBy5(a, b) {
+  return (a * b) / 5;
+}
+var multiplyAndDivideBy5Results = multiplyAndDivideBy5(35, 10);
+console.log('multiplyAndDivideBy5 exercise: ', multiplyAndDivideBy5Results);
+
+function subtractTwoNumbers(a, b) {
+  return a - b;
+}
+var subtractTwoNumbersResults = subtractTwoNumbers(22, 7);
+console.log('subtractTwoNumbers exercsie: ', subtractTwoNumbersResults);
+
+function getCircleCircumference(r) {
+  return 2 * 3.14 * r;
+}
+var getCircleCircumferenceResults = getCircleCircumference(5);
+console.log('getCircleCircumference exercise: ', getCircleCircumferenceResults);
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+var getFullNameResults = getFullName('Timothy', 'Lee');
+console.log('getFullName exercise: ', getFullNameResults);
+
+function cube(c) {
+  return c * c * c;
+}
+var cubResults = cube(5);
+console.log('cube exercise: ', cubResults);
